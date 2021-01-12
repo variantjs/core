92253ebd136bf72a09929369d2a5df2c28e654ca
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-01-12 16:27:44 -0600

    Function test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-export const Greeter = (name: string): string => {
-  return `Hello ${name}`
+export type CSSClassKeyValuePair = {
+  [key: string]: unknown
+}
+
+export type CSSClasses = CSSClass[]
+
+export type CSSClass = string | CSSClassKeyValuePair | CSSClasses | undefined
+
+export const selectClasses = (classesObject: CSSClassKeyValuePair): CSSClasses => {
+  return Object.keys(classesObject).filter((className: string) => {
+    return !!classesObject[className]
+  })
+}
+
+export const mergeClasses = (...classes: CSSClasses): string => {
+  return classes
+    .map((className: CSSClass): string => {
+      if (typeof className === 'string' || className === undefined) {
+        return className || ''
+      }
+
+      if (Array.isArray(className)) {
+        return mergeClasses(...className)
+      }
+
+      return mergeClasses(...selectClasses(className))
+    })
+    .join(' ')
+    .replace(/  +/g, ' ')
+    .trim()
 }
