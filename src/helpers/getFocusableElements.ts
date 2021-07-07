b3f282547ae015f4532e7eaf7491c4e706ac68da
alfonsobries <alfonsobries@gmail.com>
2021-07-07 17:15:53 -0500

    Use HtmlElements

--- src/helpers/getFocusableElements.ts
+++ src/helpers/getFocusableElements.ts
@@ -1,5 +1,7 @@
-const getFocusableElements = (element: Element): Array<Element> => Array.from(element.querySelectorAll(
-  'a, button, input, textarea, select, details, [contenteditable], [tabindex]:not([tabindex="-1"])',
-)).filter((el) => !el.hasAttribute('disabled'));
+const getFocusableElements = (element: HTMLElement): Array<HTMLElement> => Array
+  .from(element.querySelectorAll(
+    'a, button, input, textarea, select, details, [contenteditable], [tabindex]:not([tabindex="-1"])',
+  ))
+  .filter((el) => !el.hasAttribute('disabled')) as Array<HTMLElement>;
 
 export default getFocusableElements;
