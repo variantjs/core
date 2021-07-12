4883902a5bfb716078f043a5feeea982d55aa875
alfonsobries <alfonsobries@gmail.com>
2021-07-12 18:14:05 -0500

    No icons on core library

--- src/config/TAlertConfig.ts
+++ src/config/TAlertConfig.ts
@@ -5,7 +5,7 @@ const TAlertConfig = {
     wrapper: 'relative flex items-center p-4 border-l-4 border-blue-500 rounded shadow-sm bg-blue-50',
     body: 'flex-grow',
     close: 'relative flex items-center justify-center flex-shrink-0 w-6 h-6 ml-4 text-blue-500 transition duration-100 ease-in-out rounded hover:bg-blue-200 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50',
-    closeIcon: 'fill-current h-4 w-4',
+    closeIcon: 'h-4 w-4',
     ...enterAndLeave,
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { default as mergeClasses } from './mergeClasses';
 export { default as parseVariant } from './parseVariant';
 export { default as parseVariantWithClassesList } from './parseVariantWithClassesList';
 export { default as normalizeOptions } from './normalizeOptions';
-export * from './icons';
 export * from './config';
 export * from './helpers';
 export * from './types';
