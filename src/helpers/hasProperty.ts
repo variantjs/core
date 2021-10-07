6fc35137ac154c35980a5e066b71959c307e8b13
alfonsobries <alfonsobries@gmail.com>
2021-10-07 09:52:15 -0500

    linting issues

--- src/helpers/hasProperty.ts
+++ src/helpers/hasProperty.ts
@@ -1,3 +1,5 @@
-const hasProperty = <X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> => Object.prototype.hasOwnProperty.call(obj, prop);
+import { Data } from '../types/Misc';
+
+const hasProperty = <X extends Data, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> => Object.prototype.hasOwnProperty.call(obj, prop);
 
 export default hasProperty;
