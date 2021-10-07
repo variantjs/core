1a56e81a65c46ba495d35b3f1c5d3757a5f9da9c
alfonsobries <alfonsobries@gmail.com>
2021-10-07 09:55:00 -0500

    Update hasProperty.ts

--- src/helpers/hasProperty.ts
+++ src/helpers/hasProperty.ts
@@ -1,5 +1,4 @@
-import { Data } from '../types/Misc';
-
-const hasProperty = <X extends Data, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> => Object.prototype.hasOwnProperty.call(obj, prop);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const hasProperty = <X extends any, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> => Object.prototype.hasOwnProperty.call(obj, prop);
 
 export default hasProperty;
