2dfb5147c6b21415f19577e626b052d569d87226
alfonsobries <alfonsobries@gmail.com>
2021-10-15 11:36:46 -0500

    Input validation error and class adjustments

--- src/config/TDialogConfig.ts
+++ src/config/TDialogConfig.ts
@@ -67,7 +67,9 @@ const TDialogConfig = {
     okButton: 'block w-full max-w-xs px-4 py-2 text-white transition duration-100 ease-in-out bg-blue-500 border border-transparent rounded shadow-sm hover:bg-blue-600 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
 
     // @tw
-    inputWrapper: 'flex mt-3',
+    inputWrapper: 'mt-3',
+    // @tw
+    inputValidationError: 'mt-1 text-sm font-semibold text-red-600',
     input: TInputConfig.classes,
 
     // @tw
