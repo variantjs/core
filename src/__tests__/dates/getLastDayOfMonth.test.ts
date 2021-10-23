d300e87bed2bd2cc8725d2835c3bf1e67672d71f
alfonsobries <alfonsobries@gmail.com>
2021-10-22 19:22:07 -0500

    getLastDayOfMonth helper

---
+++ src/__tests__/dates/getLastDayOfMonth.test.ts
@@ -0,0 +1,38 @@
+import getLastDayOfMonth from '../../dates/getLastDayOfMonth';
+
+describe('getLastDayOfMonth', () => {
+  it('gets the last day of a month that have 31 days', () => {
+    // 2020-01-15
+    const date = new Date(2020, 0, 15);
+
+    expect(getLastDayOfMonth(date)).toEqual(new Date(2020, 0, 31));
+  });
+
+  it('gets the last day of a month that have 28 days', () => {
+    // 2020-02-15
+    const date = new Date(2020, 1, 15);
+
+    expect(getLastDayOfMonth(date)).toEqual(new Date(2020, 1, 29));
+  });
+
+  it('gets the last day of a month that have 30 days', () => {
+    // 2020-04-15
+    const date = new Date(2020, 3, 15);
+
+    expect(getLastDayOfMonth(date)).toEqual(new Date(2020, 3, 30));
+  });
+
+  it('gets the last day of a month if pass the last day', () => {
+    // 2020-04-30
+    const date = new Date(2020, 3, 30);
+
+    expect(getLastDayOfMonth(date)).toEqual(new Date(2020, 3, 30));
+  });
+
+  it('gets the last day of a month if pass the first day', () => {
+    // 2020-04-30
+    const date = new Date(2020, 3, 1);
+
+    expect(getLastDayOfMonth(date)).toEqual(new Date(2020, 3, 30));
+  });
+});
---
+++ src/dates/getLastDayOfMonth.ts
@@ -0,0 +1,3 @@
+const getLastDayOfMonth = (fromDate: Date): Date => new Date(fromDate.getFullYear(), fromDate.getMonth() + 1, 0);
+
+export default getLastDayOfMonth;
