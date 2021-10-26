aa9415ac32dcd473d66f66e4d05460c96450f761
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-10-26 14:38:21 +0200

    fix: export TTag class keys

--- src/config/TTagConfig.ts
+++ src/config/TTagConfig.ts
@@ -5,4 +5,8 @@ const TTagConfig = {
   },
 };
 
+export const TTagClassesKeys = Object.keys(TTagConfig.classes);
+
+export type TTagClassesValidKeys = keyof typeof TTagConfig.classes;
+
 export default TTagConfig;
