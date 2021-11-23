f1f258398acf9573d79cab1bbd333bd1b1d22902
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-11-23 09:21:19 +0100

    fix: remove popper warning

--- src/config/transitions.ts
+++ src/config/transitions.ts
@@ -1,13 +1,13 @@
 // eslint-disable-next-line import/prefer-default-export
 export const enterAndLeave = {
   // @tw
-  enterActiveClass: 'transition duration-100 ease-out',
+  enterActiveClass: 'transition-opacity duration-100 ease-out',
   // @tw
   enterFromClass: 'transform scale-95 opacity-0',
   // @tw
   enterToClass: 'transform scale-100 opacity-100',
   // @tw
-  leaveActiveClass: 'transition duration-75 ease-in',
+  leaveActiveClass: 'transition-opacity duration-75 ease-in',
   // @tw
   leaveFromClass: 'transform scale-100 opacity-100',
   // @tw
